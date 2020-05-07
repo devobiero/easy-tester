85590323ff6cb5cc96909eb366c0ffc051014e35
George Obiero <george.obiero@andela.com>
2020-05-07 22:30:41 +0300

    bump version

--- src/utils/findDirPath.ts
+++ src/utils/findDirPath.ts
@@ -2,5 +2,9 @@ import path from 'path';
 import { Config } from '../types';
 
 export const findPath = (config: Config) => {
-  return path.join(__dirname, '../../', config.rootDir);
+  return path.join(
+    // @ts-ignore
+    process.mainModule.paths[0].split('node_modules')[0].slice(0, -1),
+    config.rootDir,
+  );
 };
